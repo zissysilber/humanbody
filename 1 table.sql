ea1e4895ce0988aec7745dfc166303ce63fd6a51
zissysilber <zissysilber@gmail.com>
2023-11-13 13:03:46 -0500

    added sample data

--- 1 table.sql
+++ 1 table.sql
@@ -1,3 +1,6 @@
 Use humanbodyDB
 go
-create table dbo.body(id int not null identity primary key)
+create table dbo.body(id int not null identity primary key,
+bodypart varchar(100) not null,
+num int not null
+)
---
+++ 2 sample data.sql
@@ -0,0 +1,7 @@
+insert body(bodypart, num)
+select 'eyes', 2
+union select 'ears', 2
+union select 'nose', 1
+union select 'mouth', 2
+union select 'teeth', 32
+union select 'toungue', 1
