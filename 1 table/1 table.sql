794689cb819224c79c54aee37351889e67fa3a29
zissysilber <zissysilber@gmail.com>
2023-11-20 12:04:36 -0500

    1st commit for trial branch

--- 1 table/1 table.sql
+++ 1 table/1 table.sql
@@ -2,7 +2,8 @@ Use humanbodyDB
 go
 drop table body
 create table dbo.body(
-id int not null identity primary key,
-bodypart varchar(100) not null,
-num int not null
+	id int not null identity primary key,
+	creaturetype varchar(25) not null,
+	bodypart varchar(100) not null,
+	num int not null
 )
