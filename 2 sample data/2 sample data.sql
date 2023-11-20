0dbb393243321cdb73f225d53931144e083b4c4d
zissysilber <105228464+zissysilber@users.noreply.github.com>
2023-11-20 12:13:05 -0500

    Update 2 sample data.sql

--- 2 sample data/2 sample data.sql
+++ 2 sample data/2 sample data.sql
@@ -2,7 +2,7 @@ insert body(bodypart, num)
 select 'eyes', 2
 union select 'ears', 2
 union select 'nose', 1
-union select 'mouth', 2
+union select 'muoth', 2
 union select 'teeth', 32
 union select 'toungue', 1
 
