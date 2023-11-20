7fe3c80bd6347c4f2efe973b4f9e1bbec1364ae6
zissysilber <zissysilber@gmail.com>
2023-11-20 11:31:41 -0500

    changed using db to use db

--- 0 database.sql
+++ 0 database.sql
@@ -1,4 +1,9 @@
-
+use master
+go
+drop database if exists humanbodydb
+go
 create database humanbodydb
 go
-using humanbodydb
+use humanbodydb
+
+
--- 1 table.sql
+++ 1 table.sql
@@ -1,6 +1,8 @@
 Use humanbodyDB
 go
-create table dbo.body(id int not null identity primary key,
+drop table body
+create table dbo.body(
+id int not null identity primary key,
 bodypart varchar(100) not null,
 num int not null
 )
